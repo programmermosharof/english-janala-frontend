d255250f9daf928aa251e5358936442d70b388dc
programmermosharof <mosharof.dev@gmail.com>
2026-03-05 08:30:26 +0600

    Responsive Done

--- test.js
+++
@@ -1,7 +0,0 @@
-const createElements = (arr) => {
-    const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
-    console.log(htmlElements.join(""));
-}
-
-const nam = [ 'Mosharof', 'Munsef', 'Fahim'];
-createElements(nam)
